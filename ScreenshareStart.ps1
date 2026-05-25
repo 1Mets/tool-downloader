1298b883ecf402e3ee179f6bacde8e8f5c42d747
1Mets <jaimemichael006@gmail.com>
2026-05-24 19:01:18 -0700

    Update ScreenshareStart.ps1

--- ScreenshareStart.ps1
+++ ScreenshareStart.ps1
@@ -5,8 +5,7 @@ function Start-PersistentScript {
         [string]$Url
     )
 
-    $command = "powershell -NoExit -ExecutionPolicy Bypass -Command `"try { iex (irm '$Url') } catch { Write-Host `$_ -ForegroundColor Red }; Write-Host ''; Write-Host 'Keeping window open... Do NOT close the CMD window, all progress will be lost' -ForegroundColor Cyan; while (`$true) { Start-Sleep 3600 }`""
-
+    $command = "powershell -NoExit -ExecutionPolicy Bypass -Command `"try { iex (irm '$Url') }"
     Start-Process cmd.exe -Verb RunAs -ArgumentList "/k $command"
 }
 
